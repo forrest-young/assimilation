12c4a9251c20180b628e1332b8b228a06ab9276c
forrest-young <csssucks@gmail.com>
2017-07-11 00:38:13 -0400

    ''

--- EJ-ch4.js
+++ EJ-ch4.js
@@ -1,14 +1,20 @@
 // Range function takes (ideally) an integer
-// start and an integer end and returns an array
+// start and an integer end with an optional step
+// (default = 1) and returns an array
 // containing each integer in-between.
-function range(start, end){
+function range(start, end, step){
   if (start > end){
     var temp = start
     start = end
     end = temp
   }
-
+  if (step == undefined || step == null){
+    i = 1
+  }else{
+    i = step
+  }
   var list = []
+
 
   for (var i = 0; i <= (end - start); i++){
     list.push(start+i)
@@ -18,7 +24,10 @@ function range(start, end){
 }
 
 console.log(range(1,5))
-console.log(range(5,1))
+console.log(range(5,1) + "\n")
+console.log(range(1,5,2))
+console.log(range(5,1,2) + "\n")
+
 
 function sum(array){
   var total = 0
@@ -31,3 +40,6 @@ function sum(array){
 }
 
 console.log(sum(range(1,5)))
+console.log(sum(range(5,1) + "\n"))
+console.log(sum(range(1,5,2)))
+console.log(sum(range(5,1,2) + "\n"))
