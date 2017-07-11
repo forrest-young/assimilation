09061d9c7bf6925d44bdd6511ef9eb27a8ee6964
forrest-young <csssucks@gmail.com>
2017-07-11 00:29:49 -0400

    ''

--- EJ-ch4.js
+++ EJ-ch4.js
@@ -1,4 +1,13 @@
+// Range function takes (ideally) an integer
+// start and an integer end and returns an array
+// containing each integer in-between.
 function range(start, end){
+  if (start > end){
+    var temp = start
+    start = end
+    end = temp
+  }
+
   var list = []
 
   for (var i = 0; i <= (end - start); i++){
