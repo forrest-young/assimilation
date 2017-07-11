ef7fc5494a9b313ec406421a727b7dfc76715911
forrest-young <csssucks@gmail.com>
2017-07-11 00:18:57 -0400

    sum of a range

--- EJ-ch4.js
+++ EJ-ch4.js
@@ -9,3 +9,15 @@ function range(start, end){
 }
 
 console.log(range(1,5))
+
+function sum(array){
+  var total = 0
+
+  for (var i = 0; i < array.length; i++){
+    total += array[i]
+  }
+
+  return total
+}
+
+console.log(sum(range(1,5)))
