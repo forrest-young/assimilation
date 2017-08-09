7fd06f33adad2602f8ed5c3e111cbe7d44e75bdc
forrest-young <csssucks@gmail.com>
2017-08-08 23:54:49 -0400

    Stretch Cell

---
+++ ch6_stretchcell.js
@@ -0,0 +1,41 @@
+function TextCell(text) {
+  this.text = text.split("\n")
+}
+
+function repeat(string, times) {
+  var result = ""
+  for (var i = 0; i < times; i++)
+    result += string
+  return result
+}
+
+function StretchCell(inner, width, height){
+  this.inner = inner
+  this.width = this.inner.reduce(function (width, line) {
+    return Math.max(width, line.length)
+  }
+  if (height < 1){
+    this.height = 1
+  }else{
+    this.height = height
+  }
+}
+StretchCell.prototype.minHeight = function(){
+  return this.height
+}
+StretchCell.prototype.minWidth = function(){
+  return this.width
+}
+StretchCell.prototype.draw = function(){
+  var result = []
+  for (var i = 0; i < this.height; i++) {
+    var line = this.inner[i] || ""
+    result.push(line + repeat(" ", this.width - line.length));
+  }
+  return result
+}
+
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+console.log(sc.minWidth())
+console.log(sc.minHeight())
+console.log(sc.draw())
