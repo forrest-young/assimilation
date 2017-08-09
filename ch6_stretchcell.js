4b7133d586393e9d32a50fe6fa1b13cfa79941a9
forrest-young <csssucks@gmail.com>
2017-08-09 00:04:08 -0400

    ''

--- ch6_stretchcell.js
+++ ch6_stretchcell.js
@@ -12,7 +12,6 @@ function repeat(string, times) {
 function StretchCell(inner, width, height){
   this.inner = inner.text
   this.width = Math.max(inner.text.length, width)
-  console.log(inner.text.length)
   if (height < 1){
     this.height = 1
   }else{
